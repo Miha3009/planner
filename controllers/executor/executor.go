f101f31346015bad8138483398430f0e8e6440f4
Smirnov Mihail <masmirnov-4@yandex.ru>
2022-03-09 15:19:21 +0300

    v0.1.0

--- controllers/executor/executor.go
+++ controllers/executor/executor.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package transferer
+package executor
 
 import (
 	types "github.com/miha3009/planner/controllers/types"
